bf58f5cd6406f9bc5922d47e7d663e2475f14849
wiji <wiji.permana@gmail.com>
2022-06-24 22:59:41 +0700

    Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,66 @@
+import { Container, Navbar as NavbarBs, Nav, Button } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <NavbarBs sticky='top' className='bg-white shadow-sm mb-3'>
+      <Container>
+        <Nav>
+          <Nav.Link to={"/"} as={NavLink}>
+            Home
+          </Nav.Link>
+          <Nav.Link to={"/store"} as={NavLink}>
+            Store
+          </Nav.Link>
+          <Nav.Link to={"/about"} as={NavLink}>
+            About
+          </Nav.Link>
+        </Nav>
+        <Button
+          style={{ width: "48px", height: "48px", position: "relative" }}
+          variant='outline-primary'
+          className='rounded-circle'
+        >
+          <svg
+            width='24'
+            height='24'
+            viewBox='0 0 24 24'
+            fill='none'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path
+              fillRule='evenodd'
+              clipRule='evenodd'
+              d='M5.79166 2H1V4H4.2184L6.9872 16.6776H7V17H20V16.7519L22.1932 7.09095L22.5308 6H6.6552L6.08485 3.38852L5.79166 2ZM19.9869 8H7.092L8.62081 15H18.3978L19.9869 8Z'
+              fill='currentColor'
+            />
+            <path
+              d='M10 22C11.1046 22 12 21.1046 12 20C12 18.8954 11.1046 18 10 18C8.89543 18 8 18.8954 8 20C8 21.1046 8.89543 22 10 22Z'
+              fill='currentColor'
+            />
+            <path
+              d='M19 20C19 21.1046 18.1046 22 17 22C15.8954 22 15 21.1046 15 20C15 18.8954 15.8954 18 17 18C18.1046 18 19 18.8954 19 20Z'
+              fill='currentColor'
+            />
+          </svg>
+          <div
+            className='rounded-circle bg-danger d-flex justify-content-center align-items-center'
+            style={{
+              color: "#fff",
+              width: "24px",
+              height: "24px",
+              position: "absolute",
+              right: "0",
+              bottom: "0",
+              transform: "translate(25%, 25%)",
+            }}
+          >
+            3
+          </div>
+        </Button>
+      </Container>
+    </NavbarBs>
+  );
+}
+
+export default Navbar;
