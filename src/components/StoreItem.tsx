6ec57c8ff91f5d679bad06d185c07f45e59e5ab0
wiji <wiji.permana@gmail.com>
2022-06-25 13:00:39 +0700

    Store Page

---
+++ src/components/StoreItem.tsx
@@ -0,0 +1,57 @@
+import { Card, Button } from "react-bootstrap";
+import { formatCurrency } from "../utilities/formatCurrency";
+
+type StoreItemProps = {
+  id: number;
+  name: string;
+  price: number;
+  imgUrl: string;
+};
+
+function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
+  const quantity = 0;
+
+  return (
+    <Card className='h-100'>
+      <Card.Img
+        variant='top'
+        src={imgUrl}
+        height='200px'
+        style={{
+          objectFit: "cover",
+        }}
+      />
+      <Card.Body className='d-flex flex-column'>
+        <Card.Title className='d-flex justify-content-between align-items-baseline mb-4'>
+          <div className='fs-2'>{name}</div>
+          <div className='ms-2 text-muted'>{formatCurrency(price)}</div>
+        </Card.Title>
+        <div className='mt-auto'>
+          {quantity === 0 ? (
+            <Button className='w-100'>+ Add to Cart</Button>
+          ) : (
+            <div
+              className='d-flex align-items-center flex-column'
+              style={{ gap: "0.5rem" }}
+            >
+              <div
+                className='d-flex align-items-center justify-content-center'
+                style={{ gap: "0.5rem" }}
+              >
+                <Button>-</Button>
+                <div>
+                  <span className='fs-3'>{quantity}</span>
+                  &nbsp; in cart
+                </div>
+                <Button>+</Button>
+              </div>
+              <Button variant='danger'>Remove</Button>
+            </div>
+          )}
+        </div>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default StoreItem;
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,6 +1,20 @@
+import storedItems from "../data/items.json";
+import { Col, Row } from "react-bootstrap";
+import StoreItem from "../components/StoreItem";
+
 function Page() {
   return (
-    <h1>Store</h1>
+    <>
+      <h1>Store</h1>
+      <Row md={2} xs={1} lg={3} className='g-3'>
+        {storedItems.map(item => (
+          <Col key={item.id}>
+            {/* {JSON.stringify(item)} */}
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
   );
 }
 
