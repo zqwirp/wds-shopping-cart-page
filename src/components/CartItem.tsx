00ef3d359b9c5379576237474771045e54c3dc1a
wiji <wiji.permana@gmail.com>
2022-06-26 17:18:39 +0700

    Shopping cart component

---
+++ src/components/CartItem.tsx
@@ -0,0 +1,49 @@
+import { useShoppingCart } from "../contexts/ShoppingCartContext";
+import storeItems from "../data/items.json";
+import { Stack, Button } from "react-bootstrap";
+import { formatCurrency } from "../utilities/formatCurrency";
+
+type CartItemProps = {
+  id: number;
+  quantity: number;
+};
+
+function CartItem({ id, quantity }: CartItemProps) {
+  const { removeFromCart } = useShoppingCart();
+
+  const item = storeItems.find(item => item.id === id);
+  if (item == null) return null;
+
+  return (
+    <Stack direction='horizontal' gap={2} className='d-felx align-items-center'>
+      <img
+        src={item.imgUrl}
+        alt={item.name}
+        style={{ width: "125px", height: "75px", objectFit: "cover" }}
+      />
+      <div className='me-auto'>
+        <div>
+          {item.name}{" "}
+          {quantity > 1 && (
+            <span className='text-muted' style={{ fontSize: "0.65rem" }}>
+              {quantity}x
+            </span>
+          )}
+        </div>
+        <div className='text-muted' style={{ fontSize: "0.75rem" }}>
+          {formatCurrency(item.price)}
+        </div>
+      </div>
+      <div>{formatCurrency(item.price * quantity)}</div>
+      <Button
+        variant='outline-danger'
+        size='sm'
+        onClick={() => removeFromCart(item.id)}
+      >
+        &times;
+      </Button>
+    </Stack>
+  );
+}
+
+export default CartItem;
